a355a8dc43fa47a3ef8e65d5c0bebf18aee2715b
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-09-06 18:46:01 -0300

    Initial project structure created

---
+++ Customer.WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Customer.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+        private static readonly string Message = "Customer CRUD";
+
+        public BaseController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Message);
+        }
+    }
+}
